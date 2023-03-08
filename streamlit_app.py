97822c182d6cf85defda9573d26c2ad3fac0b6a8
TomJohnH <thasiow@onet.pl>
2023-03-08 11:31:47 +0100

    added atributes information

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,3 +100,21 @@ with tab4:
     )
 
     edited_df = st.experimental_data_editor(df)
+    st.write("**Attribute Information**")
+    st.write(
+        """<ul>
+  <li>Age: age of the patient [years]</li>
+  <li>Sex: sex of the patient [M: Male, F: Female]</li>
+  <li>ChestPainType: chest pain type [TA: Typical Angina, ATA: Atypical Angina, NAP: Non-Anginal Pain, ASY: Asymptomatic]</li>
+  <li>RestingBP: resting blood pressure [mm Hg]</li>
+  <li>Cholesterol: serum cholesterol [mm/dl]</li>
+  <li>FastingBS: fasting blood sugar [1: if FastingBS &gt; 120 mg/dl, 0: otherwise]</li>
+  <li>RestingECG: resting electrocardiogram results [Normal: Normal, ST: having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of &gt; 0.05 mV), LVH: showing probable or definite left ventricular hypertrophy by Estes' criteria]</li>
+  <li>MaxHR: maximum heart rate achieved [Numeric value between 60 and 202]</li>
+  <li>ExerciseAngina: exercise-induced angina [Y: Yes, N: No]</li>
+  <li>Oldpeak: oldpeak = ST [Numeric value measured in depression]</li>
+  <li>ST_Slope: the slope of the peak exercise ST segment [Up: upsloping, Flat: flat, Down: downsloping]</li>
+  <li>HeartDisease: output class [1: heart disease, 0: Normal]</li>
+</ul>""",
+        unsafe_allow_html=True,
+    )
